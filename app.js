03c277a616f8df4bc26e600dc95083fc2e6b3dcd
saipratap99 <122005067@sastra.ac.in>
2020-07-04 10:01:01 +0530

    Get data from user and store it in Controller and DOM selectors object is created

--- app.js
+++ app.js
@@ -17,19 +17,45 @@ var budgetController = (function(){
 // UI controller handles all the UI changes 
 // This controller doesnt know aboth other modules available
 var UIController= (function(){
-    // some code
+    var DOMstrings = {
+        add_type: '.add__type',
+        add_desc: '.add__description',
+        add_value: '.add__value',
+        add_btn: '.add__btn'
+    }
+    
+    return {
+        getDOMstrings: DOMstrings,
+        getData: function(){
+            // return the object with all the input data
+            return {
+                type: document.querySelector(DOMstrings.add_type).value,
+                description: document.querySelector(DOMstrings.add_desc).value,
+                value: document.querySelector(DOMstrings.add_value).value
+            }
+        }
+    };
 })();
 
 // Controlling UI and Business logic
 // This controller knows about budget module and UI module
 var controller = (function(budgetCtrl,UICtrl){
-    
+    var DOMs = UICtrl.getDOMstrings;
+    // all the events goes here
     var allEvents = function(){
-        console.log("Enter and mouse is Working. ");
+        // 1.Take input from form
+        var input;
+        input = UICtrl.getData();
+        console.log(input);
+        // 2.Add item in data
+
+        // 3.Show the data in UI
     }  
-    
-    document.querySelector('.add__btn').addEventListener('click',allEvents);
+    // mouse cick event
+    document.querySelector(DOMs.add_btn).addEventListener('click',allEvents);
+    // key press on anywhere in the documents
     document.addEventListener('keypress',function(event){
+        // key must be enter key or return key
         if(event.keyCode === 13 || event.which === 13)
         allEvents();
     });
